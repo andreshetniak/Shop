3bc0d010f31b30e48312fcd61e627a95f3f87940
Andrew <andreshetniak@users.noreply.github.com>
2020-01-23 17:55:13 +0200

    Added home page and some little features

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Interfaces;
+using Shop.ViewModels;
+
+namespace Shop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IAllCars carRepository;
+
+        public HomeController(IAllCars carRepository)
+        {
+            this.carRepository = carRepository;
+        }
+
+        public ViewResult Index()
+        {
+            var homeCars = new HomeViewModel
+            {
+                FavoriteCars = this.carRepository.GetFavCars
+            };
+            return View(homeCars);
+        }
+    }
+}
